7387fd07133643f6bc0b10cef66d42603bb1f617
Zachariah Crowell <zachariah.crowell@techtonic.com>
2019-09-05 13:20:15 -0600

    Refactored and added busboy

---
+++ zocial-functions/functions/handlers/shouts.js
@@ -0,0 +1,41 @@
+const  { db } = require('../util/admin')
+
+
+exports.getAllShouts = (req, res) => {
+    db.collection("shouts")
+      .orderBy("createdAt", "desc")
+      .get()
+      .then(data => {
+        let shouts = [];
+  
+        data.forEach(doc => {
+          shouts.push({
+            shoutId: doc.id,
+            body: doc.data().body,
+            userHandle: doc.data().userHandle,
+            createdAt: doc.data().createdAt
+          });
+        });
+        return res.json(shouts);
+      })
+      .catch(err => console.error(err));
+  }
+
+  exports.postOneShout = (req, res) => {
+    const newShout = {
+      body: req.body.body,
+      userHandle: req.user.handle,
+      createdAt: new Date().toISOString()
+    };
+  
+    db.collection("shouts")
+      .add(newShout)
+      .then(doc => {
+        res.json({ message: `document ${doc.id} created successfully` });
+      })
+      .catch(err => {
+        res.status(500).json({ error: "something went wrong" });
+        console.error(err);
+      });
+  }
+
---
+++ zocial-functions/functions/util/config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+module.exports = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID
+  };
